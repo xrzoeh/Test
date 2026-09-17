d820536f13b28cc023ab60b6bbab562ad9afece7
roswer <roswer.lopez@educa.madrid.org.com>
2026-09-17 13:18:49 +0200

    get y set de nombre

--- src/Data.java
+++ src/Data.java
@@ -2,6 +2,14 @@
 class for test
 @autor: roswer
 **/
-public class Data{
+public class Data {
+private String nombre;
+
+public String getNombre(){
+	return nombre;
+}
+public void setNombre(String nombre){
+this.nombre=nombre;
+}
 }
 
