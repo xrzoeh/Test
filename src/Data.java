dd91febd0158c54e71d7b6166dc0867ee7a52ad9
roswer <roswer.lopez@educa.madrid.org.com>
2026-09-17 13:55:49 +0200

    nuevo edit

--- src/Data.java
+++ src/Data.java
@@ -4,11 +4,12 @@ class for test
 @autor: roswer , Jaime
 **/
 public class Data {
-private String nombre;
+private String nombre="roswer";
 
   public String getNombre(){
     return nombre;
   }
+
   public void setNombre(String nombre){
   this.nombre=nombre;
   }
