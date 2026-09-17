f68e254d9710fe1b1a18e460e053a00905a0586f
unknown <jairubre@gmail.com>
2026-09-17 13:34:52 +0200

    Second edit Data

--- src/Data.java
+++ src/Data.java
@@ -1,8 +1,10 @@
-/*
-*Clase de test para saber usar git 
-*@author Jaime Ruiz Breña
-*\
+/**
+Clase de test para saber usar git 
+@author Jaime Ruiz Breña
+**\
 
 public class Data{
-
+	Public data(String nombre){
+	this.nombre=nombre
+	}
 }
